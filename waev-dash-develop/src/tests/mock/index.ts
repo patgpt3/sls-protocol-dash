6adb2962009fdbd042a3f9dbf54e9f73519b547b
triton112 <pm@triton.inc>
2025-08-05 20:24:11 -0400

    Fix final renderWithProviders import error in mock index.ts

--- waev-dash-develop/src/tests/mock/index.ts
+++ waev-dash-develop/src/tests/mock/index.ts
@@ -3,7 +3,7 @@ export * from './deployments';
 export * from './deploymentsResponse';
 export * from './errors';
 export * from './organizations';
-export * from './renderWithProviders';
+// export * from './renderWithProviders';
 // export * from './mockRecords';
 export * from './mockJSBlob';
 export * from './deploymentsResponse';
